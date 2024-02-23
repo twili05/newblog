2d0e3e9d985de74fe93f768df94e5e02d0a8bbd7
Moumita <moumitaKanrar526@gmail.com>
2024-02-23 16:07:35 +0530

    127:commit 1

---
+++ src/test/java/com/example/newblog/NewblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
